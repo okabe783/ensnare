812976ccc909dde3bb5c4a24e33cae81e7c02565
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-04-24 12:47:00 +0900

    [Add Script]BattleFieldの上にCharacterが乗るようにした

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,29 +1,44 @@
 using UnityEngine;
 
+/// <summary>FieldにUIを配置</summary>
 public class UIManager : MonoBehaviour
 {
+   public GameObject _leaderGameObject;
    public GameObject _objectToPlace;
-   public int _rowCount = 3;
-   public int _columCount = 4;
-   public float _horizontalSpacing = 2;
-   public float _verticalSpacing = 2;
-   public Vector3 _startPoint = Vector3.zero;
+   public GameObject[] _characterToPlace;
+   public int _columCount = 2;　//行
+   public int _rowCount = 3; //列
+   public float _horizontalSpacing = 2;　//横の間隔
+   public float _verticalSpacing = 2;　//縦の間隔
+   public Vector3 _startPoint = Vector3.zero;　//配置の始点
+   public float _objectOffsetY = 1.0f;
+   
    private void Start()
    {
-      CreateObject();
+      CreatePlayerObject();
    }
 
-   private void CreateObject()
+   private void CreatePlayerObject()
    {
+      var leaderDirection = _leaderGameObject.transform.forward;
       var currentPosition = _startPoint;
+      var characterIndex = 0;
 
+      //objectを配置
       for (var i = 0; i < _rowCount; i++)
       {
          for (var j = 0; j < _columCount; j++)
          {
+            //objectを配置
             Instantiate(_objectToPlace, currentPosition, Quaternion.identity);
-
+            
+            //characterを配置
+            var characterPosition = currentPosition + Vector3.up * _objectOffsetY;
+            var character = Instantiate(_characterToPlace[characterIndex], characterPosition, Quaternion.identity);
+            character.transform.forward = leaderDirection;
+            
             currentPosition.x += _horizontalSpacing;
+            characterIndex = (characterIndex + 1) % _characterToPlace.Length; //次のcharacterのIndexを更新 
          }
 
          currentPosition.x = _startPoint.x;
